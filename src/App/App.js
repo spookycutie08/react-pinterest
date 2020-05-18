fcc30962e6a92d14269c44d1438aa8bdc408120a
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-05-18 18:32:54 -0500

    added initial components

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Auth from '../components/Auth/Auth';
+import BoardContainer from '../components/BoardContainer/BoardContainer';
+import MyNavBar from '../components/MyNavbar/MyNavbar';
+// 1. import the component
+
 import './App.scss';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-danger">Pinterest!</button>
+        <h1>React Pinterest</h1>
+        <MyNavBar/>
+        {/* 2. put the component where you need it */}
+        <Auth />
+        <BoardContainer />
       </div>
     );
   }
