c6192ea639eeb53bc51236bea329cea51b79a98c
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-05-26 18:20:04 -0500

    form can open

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -7,6 +7,7 @@ import authData from '../../helpers/data/authData';
 import smash from '../../helpers/data/smash';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 
 class BoardContainer extends React.Component {
@@ -16,6 +17,7 @@ class BoardContainer extends React.Component {
 
   state = {
     boards: [],
+    formOpen: false,
   }
 
   getAllBoards = () => {
@@ -35,13 +37,16 @@ class BoardContainer extends React.Component {
   };
 
   render() {
-    const { boards } = this.state;
+    const { boards, formOpen } = this.state;
     const { setSingleBoard } = this.props;
     const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} removeBoard={this.removeBoard}/>);
 
     return (
       <div className="BoardContainer">
         <h2>Boards</h2>
+        <button className="btn btn-info" onClick={() => this.setState({ formOpen: true })}>Add New Board</button>
+        {/* this line above was written in-line function for formOpen, but could be called from elsewhere */}
+        { formOpen ? <BoardForm /> : ''}
         <div className="d-flex flex-wrap">
             {makeBoards}
         </div>
---
+++ src/components/BoardForm/BoardForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './BoardForm.scss';
+
+class BoardForm extends React.Component {
+  render() {
+    return (
+      <div className="BoardForm">
+        <h2>Hello from the formmm</h2>
+      </div>
+    );
+  }
+}
+
+export default BoardForm;
